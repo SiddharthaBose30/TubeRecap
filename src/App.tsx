db8901a89d6565f652b8b276e210ea4d4d68ca8d
SIDDHARTHA BOSE <60516898+SiddharthaBose30@users.noreply.github.com>
2024-04-29 01:24:00 +0530

    initial UI changes (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [buttonDisabled, setButtonDisabled] = useState(false);
-
+  const [transcriptData, setTranscriptData] = useState<any[]>([]);
+  const [summary, setSummary] = useState(false);
+  const [buttonVisible, setButtonVisible] = useState(true); // New state for button visibility
+  
   const popup = () => {
-    if (!buttonDisabled) {
-      setButtonDisabled(true);
-
+      setButtonVisible(false); // Hide the button
       chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         if (tabs && tabs.length > 0 && tabs[0].id !== undefined) {
           const activeTabId = tabs[0].id;
@@ -15,34 +16,51 @@ function App() {
           console.log("No active tabs found or tab ID is undefined.");
         }
       });
-
       setTimeout(() => {
-        setButtonDisabled(false);
-      }, 5000); // Re-enable button after 5 seconds
-    }
+        setSummary(true);   // Show the button after 5 seconds
+      }, 5000);
   };
 
   useEffect(() => {
     const button = document.getElementById("button1") as HTMLInputElement | null;
-    if (button) {
-      button.addEventListener("click", popup);
+    button?.addEventListener("click", popup);
+    if (!buttonVisible) {
       chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         if (request.message === "transcript_data") {
-          var receivedData = request.data;
-          console.log("Data received in popup.js:", receivedData);
+          setTranscriptData(request.data);
+          console.log(request.data);
         }
       });
 
       return () => {
-        button.removeEventListener("click", popup);
+        button?.removeEventListener("click", popup);
       };
     }
-  }, []);
+  }, [buttonVisible]);
 
   return (
-    <div className="App">
-      <input id="button1" type="button" value="clickme" disabled={buttonDisabled} />
-      {buttonDisabled && <p>Button will be re-enabled in 5 seconds</p>}
+    <div className="container">
+      <div style={{ display: 'flex', marginTop:'-20px'}}>
+        <img src={'./youtube-logo.svg'} alt="YouTube Logo" className="youtube-logo" style={{ width: '49px', height: '63.5px'}} />
+        <p style={{ marginTop: '20px', fontWeight: 'bold', fontSize: '20px', fontFamily: 'Arial, sans-serif', color: 'black', marginLeft:'1px', marginRight:'4px' }}>TubeRecap</p>
+      </div>
+      {buttonVisible && ( // Conditionally render the button based on visibility state
+        <button id="button1" style={{ marginLeft: '20px', marginTop: '0px'}}>Summarize</button>
+      )}
+      {!buttonVisible && !summary &&(
+        <div className="loading-message">
+          <p className="message">Summarizing</p>
+          <div className="loader"></div>
+        </div>
+      )}
+      <div className="summary">
+  {!buttonVisible && summary && (
+    <div className="summary-box">
+      <h2>Summary:</h2>
+      <p>{transcriptData[1]?.text}</p>
+    </div>
+  )}
+</div>
     </div>
   );
 }
